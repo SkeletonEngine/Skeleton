ae03043d25fbd7e0ef348d9dfee6c25868fa0ff2
Jack <skeleton.engine@gmail.com>
2024-12-03 11:25:50 +0000

    GLFW window hints

--- skeleton-core/src/skeleton/window/window.cpp
+++ skeleton-core/src/skeleton/window/window.cpp
@@ -6,6 +6,23 @@ namespace Skeleton {
 
 Window::Window(const ApplicationSettings& settings) {
   glfwInit();
+  
+  switch (settings.renderer.backend) {
+    case RendererBackend::kOpenGl: {
+      glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
+      glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+      glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+      glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+      glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+      break;
+    }
+    
+    case RendererBackend::kVulkan: {
+      glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+      break;
+    }
+  }
+  
   glfw_window = glfwCreateWindow(1280, 720, "Skeleton", NULL, NULL);
 }
 
