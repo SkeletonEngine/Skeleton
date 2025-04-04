bef67a41a71b4ac5e941bfb5a2f74dc36e8431a8
Jack <skeleton.engine@gmail.com>
2025-04-04 21:01:15 +0100

    Moving vma impl to its own cmake

--- skeleton/cmake/skeleton_renderer_vulkan_files.cmake
+++ skeleton/cmake/skeleton_renderer_vulkan_files.cmake
@@ -1,5 +1,4 @@
 set(SKELETON_RENDERER_VULKAN_FILES
-  src/skeleton/renderer/vulkan/vma_implementation.cpp
   src/skeleton/renderer/vulkan/vulkan_check.hpp
   src/skeleton/renderer/vulkan/vulkan_command_pool.cpp
   src/skeleton/renderer/vulkan/vulkan_core.hpp
---
+++ skeleton/vendor/gpuopen/vulkan-memory-allocator/vma_implementation.cpp
@@ -0,0 +1,4 @@
+// Copyright 2024-2025 SkeletonEngine
+
+#define VMA_IMPLEMENTATION
+#include <vk_mem_alloc.h>
