07590da773ee3c3bf7646973ad337706ceffa00e
Jack <skeleton.engine@gmail.com>
2025-04-19 00:32:07 +0100

    Adding lint steps for editor and runtime

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,1 +1,3 @@
 cpplint --recursive --linelength=120 --filter=-build/include_order,-readability/inheritance,-whitespace/indent_namespace file skeleton/src skeleton/include
+cpplint --recursive --linelength=120 --filter=-build/include_order,-readability/inheritance,-whitespace/indent_namespace file skeleton-editor/src
+cpplint --recursive --linelength=120 --filter=-build/include_order,-readability/inheritance,-whitespace/indent_namespace file skeleton-runtime/src
