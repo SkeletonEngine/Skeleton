79fdfbe05ef8c40be6bfed34a01cec125612fe47
Jack <skeleton.engine@gmail.com>
2025-04-23 15:45:23 +0100

    Loading OpenGl

--- skeleton/src/skeleton/renderer/opengl/opengl_renderer.cpp
+++ skeleton/src/skeleton/renderer/opengl/opengl_renderer.cpp
@@ -4,17 +4,28 @@
 #include "skeleton/core/core.hpp"
 
 #include <string>
+#include <sstream>
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+#include "skeleton/window/glfw/glfw_window.hpp"
 
 namespace Skeleton::OpenGl {
 
 OpenGlRenderer::OpenGlRenderer(Window* window) {
+  glfwMakeContextCurrent(dynamic_cast<GlfwWindow*>(window)->GetNativeWindowHandle());
+  gladLoadGL(glfwGetProcAddress);
 }
 
 void OpenGlRenderer::RenderFrame() {
 }
 
 std::string OpenGlRenderer::GetRendererString() const {
-  return "OpenGl";
+  std::stringstream ss;
+  ss << "Skeleton " << SK_VERSION_STRING;
+  ss << " [Renderer: OpenGl " << glGetString(GL_VERSION) << "]";
+  ss << " [GPU: " << glGetString(GL_RENDERER) << "]";
+
+  return ss.str();
 }
 
 }  // namespace Skeleton::OpenGl
