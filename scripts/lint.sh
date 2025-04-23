2d390dd4b5ad5f97a84d29d1b7ac63456f87248d
Jack <skeleton.engine@gmail.com>
2025-04-23 12:09:41 +0100

    Splitting lint scripts

--- scripts/lint.sh
+++
@@ -1,3 +0,0 @@
-cpplint --recursive --linelength=120 --filter=-build/include_order,-readability/inheritance,-whitespace/indent_namespace file skeleton/src skeleton/include
-cpplint --recursive --linelength=120 --filter=-build/include_order,-readability/inheritance,-whitespace/indent_namespace file skeleton-editor/src
-cpplint --recursive --linelength=120 --filter=-build/include_order,-readability/inheritance,-whitespace/indent_namespace file skeleton-runtime/src
---
+++ scripts/lint/lint-skeleton-editor.sh
@@ -0,0 +1,1 @@
+cpplint --recursive --linelength=120 --filter=-build/include_order,-readability/inheritance,-whitespace/indent_namespace file skeleton-editor/src
