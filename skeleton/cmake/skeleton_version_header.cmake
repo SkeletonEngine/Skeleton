c4301bfbbc70786f57836557592759f8908a34cb
Jack <skeleton.engine@gmail.com>
2025-05-16 05:20:39 +0100

    Updating regex matching for patch version

--- skeleton/cmake/skeleton_version_header.cmake
+++ skeleton/cmake/skeleton_version_header.cmake
@@ -5,11 +5,9 @@ function(get_git_version VERSION_MAJOR VERSION_MINOR VERSION_PATCH)
     COMMAND git describe --tags --abbrev=0
     RESULT_VARIABLE GIT_DESCRIBE_RESULT
     OUTPUT_VARIABLE LAST_TAG
-    ERROR_QUIET
   )
 
-  string(REGEX MATCHALL "v([0-9]+)\\.([0-9]+)" "${LAST_TAG}" MATCH)
-  message("${LAST_TAG}")
+  string(REGEX MATCHALL "v([0-9]+)\\.([0-9]+)" MATCH "${LAST_TAG}")
   if(MATCH)
     set(${VERSION_MAJOR} "${CMAKE_MATCH_1}" PARENT_SCOPE)
     set(${VERSION_MINOR} "${CMAKE_MATCH_2}" PARENT_SCOPE)
@@ -20,11 +18,13 @@ function(get_git_version VERSION_MAJOR VERSION_MINOR VERSION_PATCH)
   endif()
 
   execute_process(
-    COMMAND git rev-list --count ${LAST_TAG}..HEAD
+    COMMAND git rev-list --count "${MATCH}..HEAD"
     RESULT_VARIABLE GIT_COUNT_RESULT
     OUTPUT_VARIABLE COMMIT_COUNT
     ERROR_QUIET
   )
+  message("GIT_COUNT_RESULT = ${GIT_COUNT_RESULT}")
+  message("COMMIT_COUNT = ${COMMIT_COUNT}")
   string(STRIP "${COMMIT_COUNT}" COMMIT_COUNT)
   if(COMMIT_COUNT)
     math(EXPR PATCH "${COMMIT_COUNT}")
@@ -36,6 +36,7 @@ endfunction()
 
 # Display the version information
 get_git_version(VERSION_MAJOR VERSION_MINOR VERSION_PATCH)
+message("The semantic version inferred from git tags and commit history is v${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}")
 
 # Define a custom command to generate the version header
 add_custom_command(
@@ -43,11 +44,11 @@ add_custom_command(
   COMMAND ${CMAKE_COMMAND} -E configure_file
     "${CMAKE_CURRENT_SOURCE_DIR}/include/skeleton/core/version.hpp.in"
     "${CMAKE_CURRENT_SOURCE_DIR}/include/skeleton/core/version.hpp"
-  DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/include/skeleton/core/version.hpp.in"
+  DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}"
   VERBATIM
 )
 
 # Define a custom target for the version header
 add_custom_target(skeleton-version-header ALL
-  DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/include/skeleton/core/version.hpp"
+  DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}"
 )
