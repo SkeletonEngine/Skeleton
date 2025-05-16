38b9070026ea954f5f8235fd8c3b7edd3f09a6c6
Jack <skeleton.engine@gmail.com>
2025-05-16 21:09:07 +0100

    Camera FOV slider

--- skeleton-editor/src/skeleton-editor/gui/entity_properties_panel.cpp
+++ skeleton-editor/src/skeleton-editor/gui/entity_properties_panel.cpp
@@ -3,10 +3,12 @@
 #include "skeleton-editor/gui/entity_properties_panel.hpp"
 #include "skeleton/core/core.hpp"
 
+#include <sstream>
 #include <string>
 #include <imgui/imgui.h>
 #include <imgui/misc/cpp/imgui_stdlib.h>
 #include "skeleton/scene/components/components.hpp"
+#include "skeleton-editor/common/editor_preferences.hpp"
 #include "skeleton-editor/gui/scene_graph_panel.hpp"
 
 namespace Skeleton {
@@ -33,6 +35,26 @@ void DrawEntityPropertiesPanel(entt::registry* scene) {
   if (scene->any_of<ClearColorComponent>(entity)) {
     auto& clear_color = scene->get<ClearColorComponent>(entity).color;
     ImGui::ColorEdit4("Clear Color", clear_color.rgba);
+  }
+
+  // Display CameraComponent if it exists
+  if (scene->any_of<CameraComponent>(entity)) {
+    auto& camera = scene->get<CameraComponent>(entity);
+
+    // Display FOV in either degrees or radians
+    const int kMinFovDegrees = 30;
+    const int kMaxFovDegrees = 150;
+    const float kMinFovRadians = glm::radians(static_cast<float>(kMinFovDegrees));
+    const float kMaxFovRadians = glm::radians(static_cast<float>(kMaxFovDegrees));
+
+    if (g_use_degrees) {
+      int fov_degrees = static_cast<int>(glm::degrees(camera.fov));
+      ImGui::SliderInt("FOV", &fov_degrees, kMinFovDegrees, kMaxFovDegrees, "%d degrees");
+      camera.fov = glm::radians(static_cast<float>(fov_degrees));
+    }
+    else {
+      ImGui::SliderFloat("FOV", &camera.fov, kMinFovRadians, kMaxFovRadians, "%.2f radians");
+    }
   }
 
   ImGui::End();
