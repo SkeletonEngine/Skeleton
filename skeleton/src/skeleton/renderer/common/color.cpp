cf8e37d87c3d9aa9d3436fd72abb088718c27580
Jack <skeleton.engine@gmail.com>
2025-05-16 03:43:59 +0100

    Adding assert for number of color channels

--- skeleton/src/skeleton/renderer/common/color.cpp
+++ skeleton/src/skeleton/renderer/common/color.cpp
@@ -6,16 +6,19 @@
 namespace Skeleton {
 
 Color::Color(uint32_t color) {
-  r = ((color >> 24) & 0xFF) / 255.0f;
-  g = ((color >> 16) & 0xFF) / 255.0f;
-  b = ((color >> 8) & 0xFF)  / 255.0f;
-  a = (color & 0xFF)         / 255.0f;
+  r = ((color >> 24) & 0xff) / 255.0f;
+  g = ((color >> 16) & 0xff) / 255.0f;
+  b = ((color >> 8)  & 0xff) / 255.0f;
+  a = (color         & 0xff) / 255.0f;
 }
 
 Color::Color(float r, float g, float b, float a) : r(r), g(g), b(b), a(a) {
 }
 
 Color::Color(const std::initializer_list<float>& color) {
+  // A color should have either 3 or 4 channels
+  SK_ASSERT(color.size() == 3 || color.size() == 4);
+
   if (color.size() == 4) {
     auto it = color.begin();
     r = *(it++);
